6f9b813f76a7ec016c641306dab26dfb1242cd2b
Krzysiek Tłuszcz <krzysztof.kt.tluszcz@gmail.com>
2022-03-09 20:42:42 +0100

    update algorithms

--- lab1/gauss.cpp
+++ lab1/gauss.cpp
@@ -4,51 +4,76 @@
 using namespace std;
 
 int main(){
-    int cols = 5;
-    int rows = 5;
-    double X[5];
-    double B[5] = {10, 2, 9, 9, 3};
-    double var;
+    int size = 5;
+    float X[5];
+    float B[5] = {10, 2, 9, 9, 3};
+    float var;
+    float sum;
+    float deviation;
 
     //Gaussian elimination
+    float q = 0.2;
+    
+    float A[5][6] ={
+        {2*q*pow(10,-4), 1, 6,  9, 10},
+        {2*pow(10, -4),  1, 6,  9, 10},
+        {1,              6, 6,  8, 6},
+        {5,              9, 10, 7, 10},
+        {3,              4, 9,  7, 9,}
+    };
 
-    for(double q = 0.2; q < 5; q += 0.15){
-        double A[5][6] ={
-            {2*q*pow(10,-4), 1, 6,  9, 10},
-            {2*pow(10, -4),  1, 6,  9, 10},
-            {1,              6, 6,  8, 6},
-            {5,              9, 10, 7, 10},
-            {3,              4, 9,  7, 9,}
-        };
-        //add right side vector to main matrix
-        for(int i = 0; i < 5; i++){
-            A[i][5] = B[i];
-        }
-        //main algorithm
-        for(int i = 0; i < rows; i++){
-            for(int j = i + 1; j < rows; j++){
-                var = A[j][i] / A[i][i];
-                for(int k = 0; k < rows + 1; k++){
-                    A[j][k] = A[j][k] - var * A[i][k];
-                }
-            }
-        }
-        //calculate X vector
-        X[rows - 1] = A[rows - 1][cols] / A[rows - 1][cols - 1];
-        for(int i = rows - 2; i >= 0; i--){
-            X[i] = A[i][rows];
-            for(int j = i + 1; j < rows; j++){
-                X[i] = X[i] - A[i][j] * X[j];
-            }
-            X[i] = X[i] / A[i][i];
-        }
-        //print results
-        cout << "q = " << q << endl;
-        for(int i = 0; i < rows; i++){
-            cout << X[i] << "   ";
-        }
-        cout << endl;
-        cout << "##############" << endl; 
+    float backup_matrix[5][6] ={
+        {2*q*pow(10,-4), 1, 6,  9, 10},
+        {2*pow(10, -4),  1, 6,  9, 10},
+        {1,              6, 6,  8, 6},
+        {5,              9, 10, 7, 10},
+        {3,              4, 9,  7, 9,}
+    };
+
+    float supposed_results[5] = {0};
+
+    //add right side vector to main matrix
+    for(int i = 0; i < 5; i++){
+        A[i][5] = B[i];
     }
+    //main algorithm
+    for(int i = 0; i < size; i++){
+        for(int j = i + 1; j < size; j++){
+            var = A[j][i] / A[i][i];
+            for(int k = 0; k < size + 1; k++){
+                A[j][k] = A[j][k] - var * A[i][k];
+            }
+        }
+    }
+    //calculate X vector
+    X[size - 1] = A[size - 1][size] / A[size - 1][size - 1];
+    for(int i = size - 2; i >= 0; i--){
+        X[i] = A[i][size];
+        for(int j = i + 1; j < size; j++){
+            X[i] = X[i] - A[i][j] * X[j];
+        }
+        X[i] = X[i] / A[i][i];
+    }
+
+    
+    for(int i = 0; i < size; i++){
+        for(int j = 0; j < size; j++){
+            supposed_results[i] += backup_matrix[i][j] * X[j];
+        }
+    }
+
+    cout << "q = " << q << "    " << "wynik = " << endl;
+    for(int i = 0; i < size; i++){
+        cout << supposed_results[i] << "    ";
+    }
+    cout << endl;
+
+    sum = 0;
+    for (int i = 0; i < size; i++){
+        sum += pow(supposed_results[i] - B[i], 2);
+    }
+    deviation = sqrt(sum) / size;
+
+    cout << "odchylenie = " << deviation << endl << endl;
     return 0;
 }
--- lab1/jordan.cpp
+++ lab1/jordan.cpp
@@ -6,51 +6,76 @@ using namespace std;
 int main(){
   int cols = 5;
   int rows = 5;
-  double X[5];
-  double B[5] = {10, 2, 9, 9, 3};
-  double var;
-  double temp1;
-  double temp2;
+  float X[5];
+  float B[5] = {10, 2, 9, 9, 3};
+  float var;
+  float temp1;
+  float temp2;
+  float sum;
+  float deviation;
 
   //Jordan elimination
 
-  for(double q = 0.2; q < 5; q += 0.15){
-    double A[5][6] ={
-        {2*q*pow(10,-4), 1, 6,  9, 10},
-        {2*pow(10, -4),  1, 6,  9, 10},
-        {1,              6, 6,  8, 6},
-        {5,              9, 10, 7, 10},
-        {3,              4, 9,  7, 9,}
-    };
-    //add right side vector to main matrix
-    for(int i = 0; i < 5; i++){
-        A[i][5] = B[i];
+  float q = 0.2;
+  float A[5][6] ={
+      {2*q*pow(10,-4), 1, 6,  9, 10},
+      {2*pow(10, -4),  1, 6,  9, 10},
+      {1,              6, 6,  8, 6},
+      {5,              9, 10, 7, 10},
+      {3,              4, 9,  7, 9,}
+  };
+
+  float backup_matrix[5][6] ={
+          {2*q*pow(10,-4), 1, 6,  9, 10},
+          {2*pow(10, -4),  1, 6,  9, 10},
+          {1,              6, 6,  8, 6},
+          {5,              9, 10, 7, 10},
+          {3,              4, 9,  7, 9,}
+      };
+
+  float supposed_results[5] = {0};
+
+  //add right side vector to main matrix
+  for(int i = 0; i < 5; i++){
+      A[i][5] = B[i];
+  }
+  for(int i = 0; i < rows; i++){
+    temp1 = A[i][i];
+    for(int k = 0; k <= rows; k++){
+      A[i][k] = A[i][k] / temp1;
     }
-    for(int i = 0; i < rows; i++){
-      temp1 = A[i][i];
-      for(int k = 0; k <= rows; k++){
-        A[i][k] = A[i][k] / temp1;
-      }
-      for(int j = 0; j < rows; j++){
-        if(i != j){
-          temp2 = A[j][i];
-          for(int k = 0; k <=rows; k++){
-            A[j][k] = A[j][k] - temp2 * A[i][k];
-          }
+    for(int j = 0; j < rows; j++){
+      if(i != j){
+        temp2 = A[j][i];
+        for(int k = 0; k <=rows; k++){
+          A[j][k] = A[j][k] - temp2 * A[i][k];
         }
       }
     }
-    //calculate X vector
-    for(int i = 0; i < rows; i++){
-      X[i] = A[i][rows];
-    }
-    //print results
-    cout << "q = " << q << endl;
-    for(int i = 0; i < rows; i++){
-      cout << X[i] << "   ";
-    }
-    cout << endl;
-    cout << "##############" << endl; 
   }
+  //calculate X vector
+  for(int i = 0; i < rows; i++){
+    X[i] = A[i][rows];
+  }
+
+  for(int i = 0; i < rows; i++){
+          for(int j = 0; j < rows; j++){
+              supposed_results[i] += backup_matrix[i][j] * X[j];
+          }
+      }
+
+  cout << "q = " << q << "    " << "wynik = " << endl;
+  for(int i = 0; i < rows; i++){
+      cout << supposed_results[i] << "    ";
+  }
+  cout << endl;
+
+  sum = 0;
+  for (int i = 0; i < rows; i++){
+      sum += pow(supposed_results[i] - B[i], 2);
+  }
+  deviation = sqrt(sum) / rows;
+
+  cout << "odchylenie = " << deviation << endl << endl;
   return 0;
 }
